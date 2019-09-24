5cfe15fdc95317462db3c0ec98f3c1be7869fbdb
pearlonzo <54171320+pearlonzo@users.noreply.github.com>
2019-09-24 11:43:30 +0300

    initial commit

--- Arraysort.js
+++ Arraysort.js
@@ -1,1 +1,20 @@
+var Arraysort = [2,9,15,40,19,13,17,30,0,6,5,1,7,8,'ray','12','doc','p','e','a','r','l']
+var myObject ={'even':[],'odds':[],'chars':[]}
 
+for (var y=0; y<=Arraysort.length; y++) { 
+        if(Arraysort[y] % 2===0){
+             myObject.even.push(Arraysort[y])
+   }
+
+  else if(Arraysort[y] % 2=== 1){
+            myObject.odds.push(Arraysort[y])
+   }
+
+   else {
+        myObject.chars.push(Arraysort[y])
+   }
+} 
+
+console.log(myObject);
+    
+    
