52073972eabdca676394fefb060d5218098d5ddc
pearlonzo <54171320+pearlonzo@users.noreply.github.com>
2019-09-24 17:52:03 +0300

    initial commit

--- arraysort.test.js
+++ arraysort.test.js
@@ -1,17 +1,6 @@
-const arraySort = require('./arraysort');
-
-test('It does not accept numbers as input', () => {
-    expect(arraySort(y)).toBe('invalid input');
-});
-
-test('It does not accept strings as input', () => {
-    expect(arraySort('string')).toBe('invalid input');
-});
-
-test('It sorts a given array', () => {
-    expect(
-        arraySort([2,9,15,40,19,13,17,30,0,6,5,1,7,8,'ray','12','doc','p','e','a','r','l' ])
-        ).toEqual(
-            {'evens': [ 2, 40, 30, 0, 6, 8, '12' ], 'odds': [9, 15, 19, 13, 17, 5, 1, 7], 'chars': ['ray', 'doc', 'p', 'e', 'a', 'r', 'l',]}
-            );
-});
+const bill=require("./Arraysort")
+test('should be able to sort an array', ()=>{
+    const array = [1,2,3,4,'tests','javascript']
+    const result = {"chars": ["tests", "javascript"], "even": [2, 4], "odds": [1, 3]}
+    expect(bill(array)).toEqual(result)
+})
