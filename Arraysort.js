b91858ab613ba6911a57800dcd1b6969006dc59b
pearlonzo <54171320+pearlonzo@users.noreply.github.com>
2019-09-24 17:35:42 +0300

    initial commit

--- Arraysort.js
+++ Arraysort.js
@@ -1,11 +1,11 @@
-var Arraysort = [2,9,15,40,19,13,17,30,0,6,5,1,7,8,'ray','12','doc','p','e','a','r','l']
-var myObject ={'even':[],'odds':[],'chars':[]}
+function pearl(Arraysort){
+     var myObject ={'even':[],'odds':[],'chars':[]}
 
-for (var y=0; y<=Arraysort.length; y++) { 
+for (var y=0; y<Arraysort.length; y++) { 
         if(Arraysort[y] % 2===0){
              myObject.even.push(Arraysort[y])
    }
-
+  
   else if(Arraysort[y] % 2=== 1){
             myObject.odds.push(Arraysort[y])
    }
@@ -13,8 +13,11 @@ for (var y=0; y<=Arraysort.length; y++) {
    else {
         myObject.chars.push(Arraysort[y])
    }
-} 
+}
+return myObject; 
+}
+var a=pearl([1,4,7,"pearl",9,5,8,6,10,11,"y","p"])
+console.log(a);
+module.exports=pearl
 
-console.log(myObject);
-    
     
